bbee5bd16e7ae6da3227abb1e58d8c27e1701e8c
MatiasTassara <matiastassara59@gmail.com>
2019-07-04 11:31:41 -0300

    async

--- src/main/java/ejemploProjection/projections/ProjectionsApplication.java
+++ src/main/java/ejemploProjection/projections/ProjectionsApplication.java
@@ -2,8 +2,10 @@ package ejemploProjection.projections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ProjectionsApplication {
 
 	public static void main(String[] args) {
